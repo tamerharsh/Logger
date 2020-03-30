2a180e6419d44629b85651d10fc83c06af363228
tamerharsh <htharshu@gmail.com>
2020-03-30 15:23:57 +0530

    [#2] Defined log levels with  specifying it's severity.

---
+++ logger_lib/src/logger_lib.hpp
@@ -0,0 +1,12 @@
+#ifndef LOGGER_LIB_HPP
+#define LOGGER_LIB_HPP
+
+namespace logger
+{
+	//Defining log level with severity levels.
+	enum class LEVEL :int { OFF = 0, FATAL = 1, ERROR, WARN, DEBUG, TRACE = 5 };
+
+};
+
+#endif LOGGER_LIB_HPP
+
