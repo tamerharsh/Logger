bdb2e89c839fd06d12a49f4034894c6b4aaad699
tamerharsh <htharshu@gmail.com>
2020-03-31 13:54:57 +0530

    [#3] Implemented trace function  with logging of message and level .

--- logger_lib/src/logger_lib.hpp
+++ logger_lib/src/logger_lib.hpp
@@ -1,6 +1,8 @@
 #ifndef LOGGER_LIB_HPP
 #define LOGGER_LIB_HPP
 
+#include <string>
+
 namespace logger
 {
 	//Defining log level with severity levels.
@@ -13,15 +15,46 @@ namespace logger
 		//Ctor sets the severity level preferred by user.
 		Logging(logger::LEVEL severity_level_=logger::LEVEL::TRACE);
 
-	 private:
+		//Logs message at trace level.
+		void trace(std::string& message_);
+
+		private:
+
 		//Severity level prefered by user.
 		const LEVEL severity_level;
+		
+		//log_credentials.
+		struct  LogCredentials
+		{
+			 logger::LEVEL level;
+			 std::string message;
+		};
+
+		//stores the credentials such as LEVEL, message etc.
+		LogCredentials store_credentials(logger::LEVEL, std::string&);
+
 	};
 
 	//Ctor implementation.
 	Logging::Logging(logger::LEVEL severity_level_) :severity_level(severity_level_)
 	{
 	}
+
+	Logging::LogCredentials Logging::store_credentials(logger::LEVEL level_, std::string& message_)
+	{
+		Logging::LogCredentials credentials;
+		credentials.level = level_;
+		credentials.message = message_;
+		return  credentials;
+	}
+	
+	void Logging::trace(std::string& message_)
+	{
+		auto trace_credentials=store_credentials(logger::LEVEL::TRACE, message_);
+
+
+	}
+
 };
 
 #endif LOGGER_LIB_HPP
--- sample/src/sample.cpp
+++ sample/src/sample.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	//Creating obj of class Logging and specifying the preferred severity level to log messages.
 	logger::Logging obj(logger::LEVEL::FATAL);
-	
+
+	//Logging message at trace level.
+	obj.trace(std::string("hello"));
 	return 0;
 }
