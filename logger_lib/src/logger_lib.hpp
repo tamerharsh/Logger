173b27b98be424bf73bed19fdb0c0134534db049
tamerharsh <htharshu@gmail.com>
2020-04-06 04:11:23 +0530

    [#3] Implemented extraction of log credentials .

--- logger_lib/src/logger_lib.hpp
+++ logger_lib/src/logger_lib.hpp
@@ -1,61 +1,89 @@
 #ifndef LOGGER_LIB_HPP
 #define LOGGER_LIB_HPP
 
+#include <memory>
 #include <string>
+#include <iostream>
+
+#define FLAG_ON(...)  logger::Logging::extract_all_credentials(__DATE__,__TIME__,__FILE__,__FUNCTION__,__LINE__,__VA_ARGS__)
+#define FLAG_OFF(...)	logger::Logging::extract_minimimal_credentials(__DATE__,__TIME__)	
 
 namespace logger
 {
+
 	//Defining log level with severity levels.
 	enum class LEVEL :int { OFF = 0, FATAL = 1, ERROR, WARN, DEBUG, TRACE = 5 };
+	
+	//Logs the credentials.:date, time,file_name,function_name,line_number,logger_name,logger_level,message.
+	//Written in chronological order.	
+	struct  LogCredentials {
+		std::string date="null";
+		std::string time = "null";
+		std::string file_name = "null";
+		std::string function_name = "null";
+		int line = 0;
+		std::string logger_name = "root";
+	};
 
 	//Logging clas logs the messages at various severity levels.
 	class Logging
-	{
-	 public:
+	{ 
+		 public:
+		
 		//Ctor sets the severity level preferred by user.
-		Logging(logger::LEVEL severity_level_=logger::LEVEL::TRACE);
+		explicit Logging(logger::LEVEL severity_level_ = logger::LEVEL::TRACE);
 
-		//Logs message at trace level.
-		void trace(std::string& message_);
+		//Extract minimal info such as date and time.
+		static std::shared_ptr <logger::LogCredentials>  extract_minimimal_credentials(std::string date_, std::string time_, ...);
+
+		//Extarcts all the information about log.
+		static std::shared_ptr<logger::LogCredentials>  extract_all_credentials(std::string date_, std::string time_, std::string file_name_, std::string func_name_, int line_, ...);
+
+		//Method for capturing severity of type trace.
+		template<typename T = std::string>
+		bool trace(T msg_, std::shared_ptr<logger::LogCredentials> trace_credentials_=FLAG_OFF());
 
 		private:
-
-		//Severity level prefered by user.
-		const LEVEL severity_level;
 		
-		//log_credentials.
-		struct  LogCredentials
-		{
-			 logger::LEVEL level;
-			 std::string message;
-		};
+		//Severity level prefered by user.
+		const logger::LEVEL severity_level;
 
-		//stores the credentials such as LEVEL, message etc.
-		LogCredentials store_credentials(logger::LEVEL, std::string&);
-
-	};
-
+	};	
 	//Ctor implementation.
-	Logging::Logging(logger::LEVEL severity_level_) :severity_level(severity_level_)
+	logger::Logging::Logging(logger::LEVEL severity_level_) :severity_level(severity_level_)
 	{
-	}
-
-	Logging::LogCredentials Logging::store_credentials(logger::LEVEL level_, std::string& message_)
-	{
-		Logging::LogCredentials credentials;
-		credentials.level = level_;
-		credentials.message = message_;
-		return  credentials;
 	}
 	
-	void Logging::trace(std::string& message_)
+    std::shared_ptr<logger::LogCredentials> logger::Logging::extract_minimimal_credentials(std::string date_, std::string time_, ...)
 	{
-		auto trace_credentials=store_credentials(logger::LEVEL::TRACE, message_);
+		auto minimum_credentials_ptr =  std::make_shared<logger::LogCredentials>();
+		minimum_credentials_ptr->time = time_;
+		minimum_credentials_ptr->date = date_;
+		
+		return minimum_credentials_ptr;
+	}
 
+	std::shared_ptr<logger::LogCredentials> logger::Logging::extract_all_credentials(std::string date_, std::string time_, std::string file_name_, std::string func_name_, int line_, ...)
+	{
+		auto credentials_ptr = std::make_shared<logger::LogCredentials>();
+		
+		credentials_ptr->date = date_;
+		credentials_ptr->time = time_;
+		credentials_ptr->file_name = file_name_;
+		credentials_ptr->function_name = func_name_;
+		credentials_ptr->line = line_;
+
+		return credentials_ptr;
+	}
+
+	template<typename T>
+	bool logger::Logging::trace(T msg_, std::shared_ptr<logger::LogCredentials> trace_credentials_)
+	{
+		
+		return false;
 
 	}
 
 };
-
 #endif LOGGER_LIB_HPP
 
--- sample/src/sample.cpp
+++ sample/src/sample.cpp
@@ -1,11 +1,80 @@
 #include "logger_lib.hpp"
+#include <exception>
+#include <iostream>
 
-int main()
+//Creating obj of class Logging and specifying the preferred severity level to log messages.
+logger::Logging obj(logger::LEVEL::FATAL);
+
+//Log messages with minimum information.
+void test_func_1()
 {
-	//Creating obj of class Logging and specifying the preferred severity level to log messages.
-	logger::Logging obj(logger::LEVEL::FATAL);
+	obj.trace("helloworld ");
+}
 
-	//Logging message at trace level.
-	obj.trace(std::string("hello"));
+Logs messages with all the information.
+void test_func_2()
+{
+
+	obj.trace("ok !!", FLAG_ON());
+}
+int main(int argc,char**argv)
+{
+	std::cout << "test func 1\n";
+
+	test_func_1();
+
+	std::cout << "test func 2\n";
+	test_func_2();
+
+	obj.trace("ok");
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void hello()
+//{
+//
+//	obj.trace(std::string("hello"));
+//	std::cout << "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+//	std::cout << "function name  :" << __FUNCTION__ << std::endl;
+//	std::cout << "\n======FILESYSTEM ==========================\n" << std::endl;
+//	namespace fs = std::filesystem;
+//
+//	fs::path p = fs::current_path();
+//
+//	std::cout << "The current path " << p << " decomposes into:\n"
+//		<< "root name " << p.root_name() << '\n'
+//		<< "root directory " << p.root_directory() << '\n'
+//		<< "relative path " << p.relative_path() << '\n'
+//		<< "filename " << p.filename() << '\n';
+//	std::cout << "\n =============Method 3==================\n";
+//	LPSTR pBuf = new char(500);
+//	int len = 500;
+//	//		int bytes = GetModuleFileName(NULL, pBuf, len);
+//			//return bytes ? bytes : -1;
+//		//	std::cout << bytes;
+//			//std::cout << pBuf;
+//
+//	std::cout << "\n =============Method 4==================\n";
+//	std::cout << "fIEL NAME " << __FILE__ << std::endl;
+//	std::cout << "FUNCTION NAME " << __FUNCTION__ << std::endl;
+//	std::cout << "line  " << __LINE__ << std::endl;
+//}
