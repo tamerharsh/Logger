8e82028b74d371053987ae999ca683d60baec0d3
tamerharsh <htharshu@gmail.com>
2020-04-06 12:03:49 +0530

    [#3] Removing the commented code.

--- sample/src/sample.cpp
+++ sample/src/sample.cpp
@@ -11,7 +11,7 @@ void test_func_1()
 	obj.trace("helloworld ");
 }
 
-Logs messages with all the information.
+//Logs messages with all the information.
 void test_func_2()
 {
 
@@ -48,33 +48,3 @@ int main(int argc,char**argv)
 
 
 
-
-//void hello()
-//{
-//
-//	obj.trace(std::string("hello"));
-//	std::cout << "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
-//	std::cout << "function name  :" << __FUNCTION__ << std::endl;
-//	std::cout << "\n======FILESYSTEM ==========================\n" << std::endl;
-//	namespace fs = std::filesystem;
-//
-//	fs::path p = fs::current_path();
-//
-//	std::cout << "The current path " << p << " decomposes into:\n"
-//		<< "root name " << p.root_name() << '\n'
-//		<< "root directory " << p.root_directory() << '\n'
-//		<< "relative path " << p.relative_path() << '\n'
-//		<< "filename " << p.filename() << '\n';
-//	std::cout << "\n =============Method 3==================\n";
-//	LPSTR pBuf = new char(500);
-//	int len = 500;
-//	//		int bytes = GetModuleFileName(NULL, pBuf, len);
-//			//return bytes ? bytes : -1;
-//		//	std::cout << bytes;
-//			//std::cout << pBuf;
-//
-//	std::cout << "\n =============Method 4==================\n";
-//	std::cout << "fIEL NAME " << __FILE__ << std::endl;
-//	std::cout << "FUNCTION NAME " << __FUNCTION__ << std::endl;
-//	std::cout << "line  " << __LINE__ << std::endl;
-//}
